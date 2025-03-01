7882073d6fa78a5f4faa15f71995b185b0e8aacc
Iiro <iiro.liimatainen@tutanota.com>
2025-03-01 21:03:21 +0200

    added support for importing youtube's .cvs playlist exports

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Freetube-import
-Creates Freetube .db style playlist files from a list of youtube urls.
+Creates Freetube .db style playlist files from a list of youtube urls (.txt) or from .csv files exported from 'Google takeout'.
 
-Run the scrip with a path to a valid list of youtube urls.
+Run the scrip with a path to a valid list of youtube urls, or youtube's .csv playlist file. Then import the .db file into Freetube.
 
-      python create_db.py <path_to_file>
+      python create_db.py <file>
 
-Works without youtube api throug youtube-search. Some video names in playlists can get messed up because of this, the links and thumbnails will be the same though. Also works atleast on piped links.
+Works without youtube api throug youtube-search. Some video names in playlists can get messed up because of this
+and while the channel names are mostly ok the links to channels are broken in playlist view, the links and thumbnails will be the same though. 
+These are the costs for avoiding the hassle with google's own api.
+
+Also works atleast on piped links, probably also on lists of Invidious links and other links that follow the standard youtube url format.
 
 ###  Dependencies 
 https://pypi.org/project/youtube-search/
--- create_db.py
+++ create_db.py
@@ -6,29 +6,52 @@ import json
 from tqdm import tqdm
 
 def YT_authordata(yt_id):
-
     results = YoutubeSearch('https://www.youtube.com/watch?v='+yt_id, max_results=1).to_dict()
     return results
+
+def process_txt(path):
+    with open(path, "r") as inputfile:
+        Videos=inputfile.readlines()
+        Video_IDs=[]
+        for i in Videos:
+            id=i.split("?v=")
+            id=id[1].rstrip()
+            Video_IDs.append(id)
+    return Video_IDs
+
+def process_csv(path):
+    with open(path, "r") as inputfile:
+        Videos=inputfile.readlines()
+        Video_IDs=[]
+        data_start=False
+        for i in Videos:
+            if i.split(",")[0]=="Video ID":
+                data_start=True
+                continue
+            if data_start:
+                Video_IDs.append(i.split(",")[0])
+    return Video_IDs
+
 if 2>len(argv):
-    print("You must include a path to a valid .txt file")
+    print("You must include a path to a valid .txt file or youtube .csv playlistfile")
     exit(1)
 inputfile_name = argv[1]
 filenamesplit=inputfile_name.split(".")
 playlistname=filenamesplit[0]
-outputfile=open(playlistname+".db","w")
+playlistformat=filenamesplit[1]
 playlist_UUID=uuid.uuid4()
 current_time_ms = int(time.time() * 1000)
-
-inputfile=open(inputfile_name,"r")
-Videos=inputfile.readlines()
 Video_IDs=[]
-for i in Videos:
-    id=i.split("?v=")
-    id=id[1].rstrip()
-    Video_IDs.append(id)
+if playlistformat=="txt":
+    Video_IDs=process_txt(inputfile_name)
+elif playlistformat=="csv":
+    Video_IDs=process_csv(inputfile_name)
+else:
+    print(f"{playlistformat} is invalid file format.")
+    exit(1)
 
-inputfile.close()
-print(f"Reading file {inputfile_name}, the to be converted playlistfile has {len(Video_IDs)} entries")
+outputfile=open(playlistname+".db","w")
+print(f"Reading file {inputfile_name}, the playlistfile has {len(Video_IDs)} entries")
 print(f"writing to file {playlistname}.db")
 playlist_dict=dict(
     playlistName=playlistname,
@@ -37,8 +60,7 @@ playlist_dict=dict(
     createdAt=current_time_ms,
     lastUpdatedAt=current_time_ms
 )
-
-counter =0
+counter=0
 for i in tqdm(Video_IDs):
 #for i in Video_IDs:
     video_UUID=uuid.uuid4()
@@ -56,10 +78,9 @@ for i in tqdm(Video_IDs):
         timeAdded=current_time_ms,
         type="video",
         playlistItemId=str(video_UUID)
-
-
     )
     playlist_dict["videos"].append(video_dict)
+    counter+=1
 outputfile.write(json.dumps(playlist_dict,separators=(',', ':'))+"\n")
 outputfile.close()
-print(f"Task failed successfully! File {playlistname}.db written in file")
+print(f"Task failed successfully! {playlistname}.db written, with {counter} entries")
